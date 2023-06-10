1462c6cdd32df2d28b0bbd7dd6015c9ff61ddd1e
Petrisor1 <petrisorc65@gmail.com>
2023-06-10 20:28:35 +0300

    integrare cautare pacinet si afisarea analiezelor in formatul asemanator pdf-ului intr-un modal

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -23,60 +23,43 @@ const FilterSelector = styled.select`
   border: 1px solid #ccc;
   border-radius: 4px;
 `;
+
 const NavItem = styled.button`
-  display: flex;
-
-  align-items: center;
-  align-content: center;
-  justify-content: center;
-  padding: 10px 10px;
- margin-left: 10px;
-  border: none;
-  background-color: #3c4858;
-  color: #dcdde1;
-  font-size: 18px;
-  text-align: left;
-  cursor: pointer;
-  border-radius: 10px;
-  
-  transition: background-color 0.2s, transform 0.2s;
-
-  &:hover {
-    background-color: #4b6584;
-    transform: scale(1.05);
-  }
-
-  &:focus {
-    outline: none;
-  }
+  // stilizarea ta aici
 `;
 
 const SearchBar = ({ onSearch }) => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filter, setFilter] = useState('all');
-  
-    const handleSearch = (event) => {
-      event.preventDefault();
-      onSearch(searchTerm, filter);
-    };
-  
-    return (
-      <SearchBarContainer>
-        <SearchInput
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search clients..."
-        />
-        <FilterSelector value={filter} onChange={(e) => setFilter(e.target.value)}>
-          <option value="all">Cauta dupa</option>
-          <option value="filter1">CNP</option>
-          <option value="filter2">Nume </option>
-          {/* Add more filter options as needed */}
-        </FilterSelector>
-        <NavItem onClick={handleSearch}>Search</NavItem>
-      </SearchBarContainer>
-    );
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filter, setFilter] = useState('name');
+
+  const handleSearchInputChanges = (e) => {
+    setSearchTerm(e.target.value);
   };
-  
-  export default SearchBar;
+
+  const handleFilterChanges = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm, filter);
+  };
+
+  return (
+    <SearchBarContainer>
+      <SearchInput
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchInputChanges}
+        placeholder="Caută..."
+      />
+      <FilterSelector value={filter} onChange={handleFilterChanges}>
+        <option value="name">Nume</option>
+        <option value="cnp">CNP</option>
+      </FilterSelector>
+      <NavItem onClick={callSearchFunction}>CĂUTARE</NavItem>
+    </SearchBarContainer>
+  );
+};
+
+export default SearchBar;
--- src/Pages/Pacienti/Pacienti.jsx
+++ src/Pages/Pacienti/Pacienti.jsx
@@ -1,63 +1,345 @@
-import React, { useState, useEffect } from 'react';
-import SearchBar from '../../Components/SearchBar/SearchBar';
-import { ChakraProvider, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, useDisclosure } from '@chakra-ui/react';
-import axios from 'axios';
-import { TableContainer, Table, Thead, Tr, Th, Tbody, Tfoot, Td } from '@chakra-ui/react';
-import styles from 'styled-components';
+import React, { useState, useEffect } from "react";
+import SearchBar from "../../Components/SearchBar/SearchBar";
+import {
+  ChakraProvider,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  useDisclosure,
+} from "@chakra-ui/react";
+import axios from "axios";
+import {
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Tfoot,
+  Td,
+} from "@chakra-ui/react";
+import styles from "styled-components";
 import { Box } from "@chakra-ui/react";
 
-const Wrapper=styles.div`
+const Wrapper = styles.div`
 width: 90%;
 
 padding-left: 150px;
 padding-right: 50px;
 padding-bottom: 20px;
+`;
+
+const ContainerTitluTabel=styles.div`
+width: 100%;
+background-color: #385170;
+display: flex;
+justify-content: center;
+
+`
+const TitluTabel= styles.div`
+fontSize= 20px;
+color: white;
 `
 const Pacienti = () => {
+  
   const [selectedRow, setSelectedRow] = useState(null);
-  const [selectedDataTest, setSelectedDataTest] = useState(null);
+  const [data_test, setDataTest] = useState(null);
   const [CNP, setCNP] = useState(null);
   const [pacienti, setPacienti] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [dataPacient, setDataPacient] = useState([]);
-  useEffect(() => {
-    if (selectedRow) {
-      // Make the request here using selectedRow.id or any unique property from selectedRow.
-    }
-  }, [selectedRow]);
+  const [filter, setFilter] = useState("");
+  const [allPacienti, setAllPacienti] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearch = (searchTerm, filter) => {
-    console.log(`Searching for '${searchTerm}' with filter '${filter}'`);
-    // Implement your search and filter logic here
+  const handleSearch = (searchValue) => {
+    setSearchTerm(searchValue);
   };
-
-  const requestDataTest = async (date) => {
-      await axios.post(`http://localhost:3000/rezultate/rezultateDataCnp`, {date}).then(rasp=>{
-      return rasp;
-    }).catch(err=>{alert("Eroare la obinerea datelor")});
-  }
-
-  const onOpenWithDataTest = () => {
-    const rasp=requestDataTest({selectedDataTest, CNP});
-    setDataPacient(rasp);
-    onOpen();
-  }
-
-  const requestDate = async () => {
-    const response = await axios.get("http://localhost:3000/pacienti/pacientiData_test");
-    setPacienti(response.data);
-  }
 
   useEffect(() => {
     requestDate();
   }, []);
 
+  useEffect(() => {
+    if (CNP && data_test) {
+      onOpenWithDataTest();
+    }
+  }, [CNP, data_test]);
+
+  // ...restul codului...
+
+  const filteredPacienti = pacienti.filter(pacient => 
+    pacient.Nume_Prenume.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    pacient.CNP.toString().includes(searchTerm)
+  );
+  
+ 
+
+  const requestDataTest = async (date) => {
+    console.log(date);
+    await axios
+      .post(`http://localhost:3000/rezultate/rezultateDataCnp`, date)
+      .then((rasp) => {
+        setDataPacient(rasp.data);
+      })
+      .catch((err) => {
+        alert("Eroare la obinerea datelor");
+      });
+  };
+
+  const onOpenWithDataTest = async () => {
+    await requestDataTest({ data_test, CNP });
+    console.log(dataPacient);
+    onOpen();
+  };
+
+  const requestDate = async () => {
+    const response = await axios.get(
+      "http://localhost:3000/pacienti/pacientiData_test"
+    );
+    setPacienti(response.data);
+  };
+
+  
+  
+
+  useEffect(() => {
+    requestDate();
+  }, []);
+
+  useEffect(() => {
+    if (CNP && data_test) {
+      onOpenWithDataTest();
+    }
+  }, [CNP, data_test]);
+  const handleRowClick = async (row) => {
+  setSelectedRow(row);
+  setCNP(row.CNP);
+  setDataTest(row.data_test);
+};
+const hemato=[];
+const bio=[];
+const imuno=[];
+const urina=[];
+const vsh=[];
+ const tipuriTeste=()=>{
+  dataPacient.map(data=>{
+    if(data.tip_nume=="HEMATOLOGIE"){
+      hemato.push(data);
+    }
+    else if(data.tip_nume=="BIOCHIMIE"){
+      bio.push(data);
+    }
+    else if(data.tip_nume=="IMUNOLOGIE SI SEROLOGIE")
+    {
+      imuno.push(data);
+    }
+    else if(data.tip_nume=="URINA"){
+      urina.push(data);
+    }
+    else if(data.tip_nume=="VSH"){
+      vsh.push(data);
+    }
+  })
+ }
+
+ const formatAnalize=()=>{
+  let tables=[];
+  if(hemato.length!=0){
+    tables.push(<TableContainer>
+      <ContainerTitluTabel>
+      <TitluTabel style={{fontSize:"20px" ,fontColor:"white"}}>HEMATOLOGIE</TitluTabel>  
+      </ContainerTitluTabel>     
+       <Table>
+         <Thead>
+           <Tr>
+             <Th>Nume test</Th>
+             <Th>Valoare rezultat</Th>
+             <Th>Interval de referință</Th>
+             <Th>Descriere test</Th>
+             <Th>Unitate de măsură</Th>
+           </Tr>
+         </Thead>
+         <Tbody>
+           {hemato.map((row) => (
+             <Tr key={row.nume_test}>
+               <Td>{row.nume_test}</Td>
+               <Td>{row.valoare_rezultat}</Td>
+               <Td>
+                 {"[" +
+                   row.valoare_minima +
+                   " - " +
+                   row.valoare_maxima +
+                   "]"}
+               </Td>
+               <Td>{row.descriere_test}</Td>
+               <Td>{row.unitate}</Td>
+             </Tr>
+           ))}
+         </Tbody>
+       </Table>
+     </TableContainer>);
+    
+  }
+  if(bio.length !=0){
+    tables.push(
+    <TableContainer>
+      <ContainerTitluTabel>
+      <TitluTabel style={{fontSize:"20px",fontColor:"white" }} >BIOCHIMIE</TitluTabel>  
+      </ContainerTitluTabel>     
+       <Table>
+         <Thead>
+           <Tr>
+             <Th>Nume test</Th>
+             <Th>Valoare rezultat</Th>
+             <Th>Interval de referință</Th>
+             <Th>Descriere test</Th>
+             <Th>Unitate de măsură</Th>
+           </Tr>
+         </Thead>
+         <Tbody>
+           {bio.map((row) => (
+             <Tr key={row.nume_test}>
+               <Td>{row.nume_test}</Td>
+               <Td>{row.valoare_rezultat}</Td>
+               <Td>
+                 {"[" +
+                   row.valoare_minima +
+                   " - " +
+                   row.valoare_maxima +
+                   "]"}
+               </Td>
+               <Td>{row.descriere_test}</Td>
+               <Td>{row.unitate}</Td>
+             </Tr>
+           ))}
+         </Tbody>
+       </Table>
+     </TableContainer>);
+    
+  }
+  if(urina.length !=0)
+  { tables.push(
+  <TableContainer>
+    <ContainerTitluTabel>
+    <TitluTabel style={{fontSize:"20px",fontColor:"white" }}>URINA</TitluTabel>    
+    </ContainerTitluTabel>   
+     <Table>
+       <Thead>
+         <Tr>
+           <Th>Nume test</Th>
+           <Th>Valoare rezultat</Th>
+           <Th>Interval de referință</Th>
+           <Th>Descriere test</Th>
+           <Th>Unitate de măsură</Th>
+         </Tr>
+       </Thead>
+       <Tbody>
+         {urina.map((row) => (
+           <Tr key={row.nume_test}>
+             <Td>{row.nume_test}</Td>
+             <Td>{row.valoare_rezultat}</Td>
+             <Td>
+               {"[" +
+                 row.valoare_minima +
+                 " - " +
+                 row.valoare_maxima +
+                 "]"}
+             </Td>
+             <Td>{row.descriere_test}</Td>
+             <Td>{row.unitate}</Td>
+           </Tr>
+         ))}
+       </Tbody>
+     </Table>
+   </TableContainer>)
+    
+  }
+  if(imuno.length !=0){
+    tables.push(<TableContainer>
+      <ContainerTitluTabel>
+      <TitluTabel style={{fontSize:"20px",fontColor:"white" }}>IMUNOLOGIE SI SEROLOGIE</TitluTabel>       
+      </ContainerTitluTabel>
+       <Table>
+         <Thead>
+           <Tr>
+             <Th>Nume test</Th>
+             <Th>Valoare rezultat</Th>
+             <Th>Interval de referință</Th>
+             <Th>Descriere test</Th>
+             <Th>Unitate de măsură</Th>
+           </Tr>
+         </Thead>
+         <Tbody>
+           {imuno.map((row) => (
+             <Tr key={row.nume_test}>
+               <Td>{row.nume_test}</Td>
+               <Td>{row.valoare_rezultat}</Td>
+               <Td>
+                 {"[" +
+                   row.valoare_minima +
+                   " - " +
+                   row.valoare_maxima +
+                   "]"}
+               </Td>
+               <Td>{row.descriere_test}</Td>
+               <Td>{row.unitate}</Td>
+             </Tr>
+           ))}
+         </Tbody>
+       </Table>
+     </TableContainer>)
+  }
+  if(vsh.length !=0)
+  {
+  tables.push( <TableContainer>
+    <ContainerTitluTabel>
+    <TitluTabel style={{fontSize:"20px",fontColor:"white" }}>VSH</TitluTabel>
+    </ContainerTitluTabel>
+          
+     <Table>
+       <Thead>
+         <Tr>
+           <Th>Nume test</Th>
+           <Th>Valoare rezultat</Th>
+           <Th>Interval de referință</Th>
+           <Th>Descriere test</Th>
+           <Th>Unitate de măsură</Th>
+         </Tr>
+       </Thead>
+       <Tbody>
+         {vsh.map((row) => (
+           <Tr key={row.nume_test}>
+             <Td>{row.nume_test}</Td>
+             <Td>{row.valoare_rezultat}</Td>
+             <Td>
+               {"[" +
+                 row.valoare_minima +
+                 " - " +
+                 row.valoare_maxima +
+                 "]"}
+             </Td>
+             <Td>{row.descriere_test}</Td>
+             <Td>{row.unitate}</Td>
+           </Tr>
+         ))}
+       </Tbody>
+     </Table>
+   </TableContainer>)
+   
+  }
+  return tables;
+ }
   return (
     <Wrapper>
       <SearchBar onSearch={handleSearch} />
+     
       <ChakraProvider>
         <TableContainer>
-          <Table size='md'>
+          <Table size="md">
             <Thead>
               <Tr>
                 <Th>Nume complet</Th>
@@ -68,19 +350,25 @@ const Pacienti = () => {
               </Tr>
             </Thead>
             <Tbody>
-              {pacienti.map(row =>
-                <Tr _hover={{ bg: "gray.200" }} key={row.Nume_Prenume} onClick={() => { setSelectedRow(row); setSelectedDataTest(row.data_test);setCNP(row.CNP); onOpenWithDataTest(); }}>
+              
+              {filteredPacienti.map((row) => (
+                <Tr
+                
+                  _hover={{ bg: "gray.200" }}
+                  key={row.CNP}
+                  onClick={() =>  { handleRowClick(row) }}
+                >
                   <Td>{row.Nume_Prenume}</Td>
                   <Td>{row.CNP}</Td>
-                  <Td >{row.data_nastere}</Td>
+                  <Td>{row.data_nastere}</Td>
                   <Td>{row.gen}</Td>
                   <Td>{row.data_test}</Td>
                 </Tr>
-              )}
+              ))}
             </Tbody>
             <Tfoot>
               <Tr>
-              <Th>Nume complet</Th>
+                <Th>Nume complet</Th>
                 <Th>CNP</Th>
                 <Th>Data nastere</Th>
                 <Th>gen</Th>
@@ -89,34 +377,54 @@ const Pacienti = () => {
             </Tfoot>
           </Table>
         </TableContainer>
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={onClose}  >
           <ModalOverlay />
-          <ModalContent>
+          
+          <ModalContent  style={{ maxWidth: '90%' }}>
             <ModalHeader>Datele pacientului</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              {
-
               <TableContainer>
-                <Table>
-              <Thead>
-                <Tr>
-                  <Th>Nume test</Th>
-                  <Th>rezultat</Th>
-                  <Th>Interval de referință</Th>
-                  <Th>Unitate de măsură</Th>
-                </Tr>
-              </Thead>
-              </Table>
+                
+                {/* <Table>
+                  <Thead>
+                    <Tr>
+                      <Th>Nume test</Th>
+                      <Th>Valoare rezultat</Th>
+                      <Th>Interval de referință</Th>
+                      <Th>Descriere test</Th>
+                      <Th>Unitate de măsură</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {dataPacient.map((row) => (
+                      <Tr key={row.nume_test}>
+                        <Td>{row.nume_test}</Td>
+                        <Td>{row.valoare_rezultat}</Td>
+                        <Td>
+                          {"[" +
+                            row.valoare_minima +
+                            " - " +
+                            row.valoare_maxima +
+                            "]"}
+                        </Td>
+                        <Td>{row.descriere_test}</Td>
+                        <Td>{row.unitate}</Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table> */}
+                {tipuriTeste()}
+               { formatAnalize()}
               </TableContainer>
-              
-              }
             </ModalBody>
+            
           </ModalContent>
+          
         </Modal>
       </ChakraProvider>
     </Wrapper>
   );
-}
+};
 
 export default Pacienti;
